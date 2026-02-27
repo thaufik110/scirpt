92d0c636868cc9e1c931fb729c5c049195bc0d56
E X O R C I S T <112650076+thaufik110@users.noreply.github.com>
2026-02-27 17:55:17 +0700

    Change theme from 'Dracula' to 'TokyoNight'

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/thaufik110/libraryzhushi/refs/heads/main/newui.lua"))()
 
-local Window = Library:CreateWindow("🌊 AQUA HUB", "Dracula")
+local Window = Library:CreateWindow("🌊 AQUA HUB", "TokyoNight")
 
 local TabInfo = Window:CreateTab("Water", "rbxassetid://3926305904")
 TabInfo:CreateSection("Berenang")
